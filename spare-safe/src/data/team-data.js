04fe4cce205ae4407a57d1acb7bc08ec59fbf8df
RanyaOUNI <136346306+RanyaOUNI@users.noreply.github.com>
2023-09-21 11:09:36 +0100

    added-file

---
+++ spare-safe/src/data/team-data.js
@@ -0,0 +1,45 @@
+export const teamData = [
+  {
+    img: "/img/team-1.jpg",
+    name: "Sousse",
+   
+   
+  },
+  {
+    img: "/img/team-2.jpg",
+    name: "Tunis",
+    
+  },
+  {
+    img: "/img/team-3.jpg",
+    name: "Sfax",
+   
+  },
+  {
+    img: "/img/team-4.jpg",
+    name: "Tozeur",  
+  },
+  {
+    img: "/img/team-5.jpg",
+    name: "Monastir",
+    
+  },
+  {
+    img: "/img/team6.jpg",
+    name: "Gafsa",
+    
+  },
+  {
+    img: "/img/team-7.jpg",
+    name: "Nabeul",
+    
+  },
+  {
+    img: "/img/team-8.jpg",
+    name: "Mahdia",
+    
+  },
+ 
+];
+
+export default teamData;
---
+++ spare-safe/src/pages/User_dashboard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../../public/css/tailwind.css'
+import {
+    Input,
+    Button
+} from "@material-tailwind/react";
+
+
+const User_dashboard = () => {
+    return (
+    <>
+        
+    
+    
+    
+    
+    </>
+    )
+}
+
+export default User_dashboard
